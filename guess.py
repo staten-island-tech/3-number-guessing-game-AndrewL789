9a3ac8e1e5791b94110331a3a6a599b1747b2022
AndrewL789 <Andrewl434@nycstudents.net>
2025-09-26 09:22:39 -0400

    done

--- guess.py
+++ guess.py
@@ -1,22 +1,46 @@
 import random
-
+#grah = random.randint(1,10)
+#replace all instances of bot with grah
 pool = [1,2,3,4,5,6,7,8,9,10]
 b1 = min(pool)
 b2 = max(pool)
-x = 1
+history = [] 
 bot = int(random.choice(pool))
 print(f" I'm thinking of a number between {b1} and {b2}, what is it?:")
-choice = int(input("What am I thinking of?:"))
-if choice == bot:
-    print("that is right")
+choice = 0
 while bot != choice:
-    
-    
-    choice = int(input("try again:"))
-    x = x + 1
-    print(f" this is attempt x{x}")
-    
-    
+    choice = int(input("guess?:"))
+    print("try again:")
+    if choice < bot:
+        history.append(choice)
+        print(f"{choice} is less than the number")
+    elif choice > bot:
+        history.append(choice)
+        print(f"{choice} is greater than the number")
     if choice == bot:
-        print(f"that is right, only took you {x + 1} attempts")
+        history.append(choice)
+        tah = len(history)
+        print(f"{choice} is correct. Only took you {tah} attempts")
+        print(history)
 
+
+""" #challenge 1 
+x = 10
+while x != 1:
+    print(x)
+    x = x - 1 
+    if x == 1:
+        print(x) """
+
+#challenge 2 
+""" 
+color = 0
+list = []
+while color != "stop":
+    color = input("what are your favorite colors? type 'stop' to stop:")
+    if color != "stop":
+        list.append(color)
+    elif color == "stop":
+        list = (set(list))
+        print(f"your favorite colors are {list}")
+     """
